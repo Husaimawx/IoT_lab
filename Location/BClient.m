90b2b21ee117b2128dfa8f5a0bb945fd7a207ca6
ukyotachibana0 <3298825551@qq.com>
2021-12-05 14:55:50 +0800

    client

--- Location/BClient.m
+++ Location/BClient.m
@@ -1,6 +1,6 @@
 %%
 % TCP连接，IP地址和端口可以自己设置，只需保证设备A和设备B一致即可
-IP = '0.0.0.0';
+IP = '183.172.227.165';
 PortN = 20000;
 
 %设备B发送调频连续波信号(chirp)，频率从6000Hz变化到8000Hz，持续0.5秒
@@ -13,12 +13,12 @@ y = chirp(t, f2, T, f3);
 % 启动TCP连接的客户端
 Client = tcpip(IP, PortN, 'NetworkRole', 'client');
 fopen(Client);
-%%
+%%                                                                                                                                                                                                                           
 Rec = audiorecorder(fs, 16, 1);
 rdy = fgetl(Client);                            %客户端收到服务端准备就绪的消息
 fprintf(Client, 'Client Ready');    %客户端发送消息给服务端，设备B准备开始发送声波
-record(Rec, T * 6);                             %设备B开始录音
-pause(T * 3);                                           %接收设备A发送的声音
+record(Rec, T * 9);                             %设备B开始录音
+pause(T * 6);                                           %接收设备A发送的声音
 soundsc(y, fs, 16);                             %设备B发送声音
 pause(T * 3);                                           %等待录音结束
 %%
@@ -32,14 +32,16 @@ z2 = chirp(t, f2, T, f3); z2 = z2(end : -1 : 1);
 [~, p2] = max(conv(recvData, z2, 'valid'));
 
 %将计算的信号起始位置差发送给服务端
-psub = num2str(p2 - p1);
+psub = num2str(p2 - p1)
 fprintf(Client, psub);
 
 %从频谱图中可以比对找到的信号开始位置是否准确
-p1 = (p1 - 1) / fs;
-p2 = (p2 - 1) / fs;
+p1 = (p1 - 1) / fs
+p2 = (p2 - 1) / fs
 hold on;
 plot([0, fs / 1000 / 2], [p1, p1], 'r-');
 plot([0, fs / 1000 / 2], [p2, p2], 'b-');
 %%
 fclose(Client);
+
+soundsc(recvData, fs, 16);
